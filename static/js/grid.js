08584933d70a30c86a5759f7df0eff29acb86e17
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-10-21 03:57:35 +0100

    Add js to clear and cleaned up event listeners

--- static/js/grid.js
+++ static/js/grid.js
@@ -1,41 +1,73 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     let isMouseDown = false;
+    
     const grid = document.getElementById('grid');
-    grid.addEventListener('mousedown', (event) => {
+    const clearButton = document.getElementById('clear');
+
+    // Event handlers
+    const handleMouseDown = (event) => {
         isMouseDown = true;
         toggleCell(event.target);
-    });
+    }
 
-    grid.addEventListener('mouseup', () => {
-        isMouseDown = false;
-    });
+    const handleMouseUp = () => { isMouseDown = false; }
 
-    grid.addEventListener('mouseover', (event) => {
+    const handleMouseOver = (event) => {
         if (isMouseDown) {
             toggleCell(event.target);
         }
-    });
+    }  
+
+    async function handleClearButtonClick() {
+        const cells = document.querySelectorAll('.filled');
+        cells.forEach(cell => {
+            cell.classList.remove('filled');
+        });
+        await clearData();
+    }
+
+    // Add event listeners
+    grid.addEventListener('mousedown', handleMouseDown );
+    grid.addEventListener('mouseup', handleMouseUp);
+    grid.addEventListener('mouseover', handleMouseOver);
+    clearButton.addEventListener('click', handleClearButtonClick);
+
+    async function clearData() {
+       await sendData(JSON.stringify({}), 'clear');
+    }
 
     async function toggleCell(cell) {
         const x = cell.getAttribute('data-x');
         const y = cell.getAttribute('data-y');
         cell.classList.toggle('filled');
-        await sendData(x, y);
-    }       
+        await sendPixelData(x, y);
+    }    
 
-    async function sendData(x, y) {
+    async function sendPixelData(x, y) {
+        sendData({ x: x, y: y }, "submit");
+    }
+
+    async function sendData(message, controller){
         try {
-            const response = await fetch('/submit', {
+            const response = await fetch(`/${controller}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ x: x, y: y }),
+                body: JSON.stringify(message),
             });
             const data = await response.json();
         } catch (error) {
             console.error('Error:', error);
         }
     }
+
+    // Clean up event listeners when the window is unloaded
+    window.addEventListener('unload', () => {
+        grid.removeEventListener('mousedown', handleMouseDown);
+        grid.removeEventListener('mouseup', handleMouseUp);
+        grid.removeEventListener('mouseover', handleMouseOver);
+        clearButton.removeEventListener('click', clearData);
+    });
 });
