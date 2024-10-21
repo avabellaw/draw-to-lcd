2d3ec9ea882624faae90c8d161ca2d1961c6a6e8
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-10-21 02:04:27 +0100

    maint: Add comments to app.py

--- app.py
+++ app.py
@@ -4,42 +4,59 @@ import json
 
 app = Flask(__name__)
 
-pixels = [[0] * 128 for _ in range(128)]
+
+class PixelGrid:
+    '''Class to represent the webpage's pixel grid and map to the lcd's display pixels'''
+    def __init__(self, size):
+        self.pixels = [[0] * size for _ in range(size)]
+        self.size = size
+
+    def refresh(self):
+        '''Get the pixels from the lcd and update the webpage's pixel grid'''
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.sendto("GET".encode(), ('192.168.1.169', 65432))
+            data, _ = s.recvfrom(65536)
+            print(f"Received data: {data}")
+            self.pixels = json.loads(data.decode())
+
+    def set_pixel(self, x, y, value):
+        '''Set a pixel on the webpage and update the lcd'''
+        self.pixels[y][x] = value
+        self.send_to_lcd(x, y, value)
+
+    def send_to_lcd(self, x, y, pixel):
+        '''Send a pixel to the lcd'''
+        message = f"{x},{y},{pixel}"
+        print(f"Sending to LCD: {message}")
+
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.sendto(message.encode(), ('192.168.1.169', 65432))
+
+    def __len__(self):
+        return self.size**2
 
 
-def get_lcd_pixels():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.sendto("GET".encode(), ('192.168.1.169', 65432))
-        data, _ = s.recvfrom(65536)
-        return json.loads(data.decode())
+pixel_grid = PixelGrid(128)
 
 
 @app.route('/')
 def home():
-    print(get_lcd_pixels())
-    return render_template('index.html', pixels=pixels)
+    '''Homepage controller'''
+    pixel_grid.refresh()
+    return render_template('index.html', pixels=pixel_grid.pixels)
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    '''Controller for handling pixel data from the webpage'''
     data = request.get_json()
     x, y = int(data.get('x')), int(data.get('y'))
 
-    pixel = 1 if pixels[y][x] == 0 else 0
-    pixels[y][x] = pixel
-    
-    send_to_lcd(x, y, pixel)
+    pixel = 1 if pixel_grid.pixels[y][x] == 0 else 0
+    pixel_grid.set_pixel(x, y, pixel)
 
     return jsonify({'status': 'success',
                     'pixel_value': f"{pixel}", 'x': x, 'y': y})
-
-
-def send_to_lcd(x, y, pixel):
-    message = f"{x},{y},{pixel}"
-    print(f"Sending to LCD: {message}")
-
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.sendto(message.encode(), ('192.168.1.169', 65432))
 
 
 if __name__ == '__main__':
